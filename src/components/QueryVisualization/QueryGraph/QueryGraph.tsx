d51616d82a501f47efa65a21a17415a6c32d7c1a
Harry Li <harry.li@ll.mit.edu>
2025-01-29 11:59:48 -0500

    added graph recenter button

--- src/components/QueryVisualization/QueryGraph/QueryGraph.tsx
+++ src/components/QueryVisualization/QueryGraph/QueryGraph.tsx
@@ -1,17 +1,20 @@
 // Copyright (c) 2024 Massachusetts Institute of Technology
 // SPDX-License-Identifier: MIT
 
-import {useMemo} from "react";
+import {useMemo, useRef} from "react";
 
 import Graphin, {Components, LegendChildrenProps} from "@antv/graphin";
 import '@antv/graphin/dist/index.css';
-import { Title } from '@mantine/core';
+import { ActionIcon, Button, Title } from '@mantine/core';
 
 import { useAppSelector } from "redux/store.ts";
 
 import { parseSparqlQuery } from "utils/parseSparqlQuery.ts";
 import { transformTripleQueryToGraphin } from "utils/transformTripleDataToGraphin.ts";
 import { useQueryGetIDTableEntitiesFromQuery } from "utils/knowledgeBase/getEntityData.ts";
+import { IconFocus } from "@tabler/icons-react";
+
+import styles from "./QueryGraph.module.scss"
 
 const { Legend } = Components;
 
@@ -26,19 +29,32 @@ export const QueryGraph = () => {
         return transformTripleQueryToGraphin(semanticTriples, idTableEntities||undefined);
     }, [queryValue, idTableEntities]);
 
+    const graphRef = useRef<Graphin>(null)
+    const center = () => {
+        if(graphRef.current) {
+            const graph = graphRef.current.graph;
+            graph.fitView(); // Re-centers and fits graph to view
+        }
+    }
+
+
     if(queryGraphData.nodes.length===0) return null
 
     return (
         <div>
             <Title style={{color:"white", marginLeft: 13, marginBottom: 7, marginTop: 7, padding: 1}} order={4}>Query Structure Graph</Title>
-            <div style={{height: '400px'}}>
-                <Graphin data={queryGraphData} layout={{type: 'dagre', rankdir: 'LR'}} style={{minHeight: "unset"}}>
+            <div id={styles["graph-container"]}>
+                <Graphin data={queryGraphData} ref={graphRef} layout={{type: 'dagre', rankdir: 'LR'}} style={{minHeight: "unset"}}>
                     <Legend bindType="node" sortKey="data.type">
                         {(renderProps: LegendChildrenProps) => {
                             return <Legend.Node {...renderProps} />;
                         }}
                     </Legend>
                 </Graphin>
+
+                <ActionIcon className={styles["recenter-button"]} size="sm" variant="filled" aria-label="Center" onClick={() => center()}>
+                    <IconFocus/>
+                </ActionIcon>
             </div>
         </div>
     )
